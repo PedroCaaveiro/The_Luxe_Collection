5b5b741533a465dfe11c7b076c71ec2afe04f6ff
PedroCaaveiro <acaaveir@yahoo.es>
2025-01-31 11:38:54 +0100

    cambios

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,28 @@
-const { src, dest, watch, series, parallel } = require('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('autoprefixer');
-const postcss = require('gulp-postcss')
-const sourcemaps = require('gulp-sourcemaps')
-const cssnano = require('cssnano');
-const concat = require('gulp-concat');
-const terser = require('gulp-terser-js');
-const rename = require('gulp-rename');
-const imagemin = require('gulp-imagemin'); // Minificar imagenes 
-const notify = require('gulp-notify');
-const cache = require('gulp-cache');
-const clean = require('gulp-clean');
-const webp = require('gulp-webp');
+const sourcemaps = require('gulp-sourcemaps');
+const { src, dest, watch, parallel } = require('gulp');
 
+// Rutas de los archivos
 const paths = {
-    scss: 'src/scss/**/*.scss',
-    js: 'src/js/**/*.js',
-    imagenes: 'src/img/**/*'
-}
+    scss: 'src/scss/**/*.scss',  // Todos los archivos .scss
+};
 
+// Función para compilar SASS a CSS
 function css() {
-    return src(paths.scss)
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        // .pipe(postcss([autoprefixer()]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('build/css'));
+    return src(paths.scss)  // Selecciona los archivos .scss
+        .pipe(sourcemaps.init())  // Inicializa los sourcemaps
+        .pipe(sass())  // Compila el archivo SASS
+        .pipe(sourcemaps.write('.'))  // Genera el sourcemap
+        .pipe(dest('build/css'));  // Guarda el archivo CSS compilado en 'build/css'
 }
 
-function javascript() {
-    return src(paths.js)
-      .pipe(sourcemaps.init())
-      .pipe(concat('bundle.js'))
-      .pipe(terser())
-      .pipe(sourcemaps.write('.'))
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(dest('./build/js'))
-}
-
-function imagenes() {
-    return src(paths.imagenes)
-        .pipe(cache(imagemin({ optimizationLevel: 3 })))
-        .pipe(dest('build/img'))
-        .pipe(notify('Imagen Completada' ));
-}
-
-function versionWebp() {
-    return src(paths.imagenes)
-        .pipe(webp())
-        .pipe(dest('build/img'))
-        .pipe(notify({ message: 'Imagen Completada' }));
-}
-
-
+// Función para observar cambios en los archivos SCSS
 function watchArchivos() {
-    watch(paths.scss, css);
-    watch(paths.js, javascript);
-    watch(paths.imagenes, imagenes);
-    watch(paths.imagenes, versionWebp);
+    watch(paths.scss, css);  // Observa los cambios en los archivos .scss
 }
 
+// Exportar las tareas
 exports.css = css;
 exports.watchArchivos = watchArchivos;
-exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos); 
+exports.default = parallel(css, watchArchivos);
